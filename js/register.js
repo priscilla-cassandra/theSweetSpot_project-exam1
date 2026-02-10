7e0f4dfa2c2dd6a38930b3e34815fd656318dcd1
priscilla-cassandra <priscilla-92@live.com>
2026-02-10 15:25:20 +0100

    Register-user logic

--- js/register.js
+++ js/register.js
@@ -3,12 +3,39 @@ import {BASE_API_URL} from "./api.js"
 const AUTH_REGISTER_URL = `${BASE_API_URL}/auth/register`
 
 const registerForm = document.getElementById("register-form")
+const errorMessageDisplay = document.getElementById("auth-error")
 
-
-const options = {
-    method: "POST",
-    body: "Some data",
-    headers: {
-        "Content-Type": "application/json"
+async function registerUser(userDetails){
+    try{
+        const options = {
+            method: "POST",
+            body: JSON.stringify(userDetails),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        const response = await fetch(AUTH_REGISTER_URL, options)
+        const result = await response.json()
+        if(!response.ok){
+            console.log(result)
+            throw new Error(result?.errors?.[0].message || response.status)
+        }
+        
+        const registerUser = result.data
+        console.log(response)
+    }catch(error){
+        errorMessageDisplay.textContent = error.message
     }
 }
+
+function onRegisterFormSubmit(event){
+event.preventDefault()
+const formData = new FormData(event.target)
+const formFields = Object.fromEntries(formData)
+registerUser(formFields)
+}
+
+registerForm.addEventListener("submit", onRegisterFormSubmit)
+
+
+
