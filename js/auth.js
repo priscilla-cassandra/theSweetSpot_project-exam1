1408028b508611c1d178add92d9a21fb51876f03
priscilla-cassandra <priscilla-92@live.com>
2026-02-12 11:00:56 +0100

    auth functions (get access token, check for access token, log out)

---
+++ js/auth.js
@@ -0,0 +1,16 @@
+//Gets the accesToken saved in local storage when user is logged in
+export function getAccessToken(){
+    return localStorage.getItem("accessToken")
+}
+
+//Checks if there is an accessToken (if a user is logged in)
+export function isLoggedIn(){
+    return !!getAccessToken() //Return "true" if there is an accessToken, return "false" if there is no accessToken
+}
+
+//When user logs out
+export function logout(){
+    localStorage.removeItem("accessToken") //Remove accessToken
+    localStorage.removeItem("username") //Remove username
+    window.location.href = "/index.html" //Redirect to the feed/home page
+}
