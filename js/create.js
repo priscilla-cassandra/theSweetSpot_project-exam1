24969d1fc168a24f59cf54c8d459738718da60c7
priscilla-cassandra <priscilla-92@live.com>
2026-02-14 18:10:37 +0100

    sucess message, input for POST request (title, body, media),catch error

--- js/create.js
+++ js/create.js
@@ -6,31 +6,61 @@ siteAuthentication()
 
 const createPostUrl = BLOG_POSTS_URL
 const createPostForm = document.getElementById("new-post")
-console.log(createPostForm)
+const publishedMessage = document.getElementById("create-error")
+
+console.log("token:", localStorage.getItem("accessToken"))
 
 async function createNewBlogPost(blogpostInput){
     const accessToken = getAccessToken() //Store current accessToken in variable
     
     try{
+        
+        const ingredients = blogpostInput.ingredients
+        const instructions = blogpostInput.instructions
+        //Combine both input fields in the form so they together can be stored in the API body
+        const blogPostBody = `## Ingredients
+        
+        ${ingredients}
+        
+        ## Instructions
+
+        ${instructions}`
+
+        const blogPostContent = {
+            title: blogpostInput.title,
+            body: blogPostBody,
+            media: {
+                url: blogpostInput.imageURL,
+                alt: blogpostInput.title
+            }
+        }
+        
         const options = {
             method: "POST",
-            body: JSON.stringify(blogpostInput),
+            body: JSON.stringify(blogPostContent),
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${accessToken}`
             }
         }
 
-        const response = await fetch (createPostUrl)
+        console.log("Payload:", blogPostContent)
+
+        const response = await fetch (createPostUrl, options)
         const result = await response.json()
 
         if(!response.ok){
             throw new Error(result?.errors?.[0].message || response.status)
         }
 
+        //If blog bost was successfully posted, do this
+        publishedMessage.textContent = "Your post was successfully published! Redirecting..."
+        publishedMessage.classList.add("success-message")
+
 
     }catch(error){
-
+        publishedMessage.textContent = error.message
+        publishedMessage.classList.add("error-message")
     }
 }
 
