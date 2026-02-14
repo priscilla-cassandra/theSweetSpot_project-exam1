11c6b08cc23293b7c2dd551ab3391af507badaf4
priscilla-cassandra <priscilla-92@live.com>
2026-02-14 11:00:53 +0100

    Fixed blog posts URL

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
 export const BASE_API_URL = "https://v2.api.noroff.dev"
 export const AUTH_REGISTER_URL = `${BASE_API_URL}/auth/register`
 export const AUTH_LOGIN_URL = `${BASE_API_URL}/auth/login`
-export const ALL_POSTS_URL = `${BASE_API_URL}/<name>`
+export const BLOG_POSTS_URL = `${BASE_API_URL}/blog/posts/priscillaneeraas`
