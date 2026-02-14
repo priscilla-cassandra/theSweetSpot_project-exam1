90156240f05821d19f4048249abd9af2dd5722ce
priscilla-cassandra <priscilla-92@live.com>
2026-02-14 10:20:36 +0100

    new const for fetching all blog posts

--- js/api.js
+++ js/api.js
@@ -1,3 +1,4 @@
 export const BASE_API_URL = "https://v2.api.noroff.dev"
 export const AUTH_REGISTER_URL = `${BASE_API_URL}/auth/register`
 export const AUTH_LOGIN_URL = `${BASE_API_URL}/auth/login`
+export const ALL_POSTS_URL = `${BASE_API_URL}/<name>`
