abfba21ce6ced3843f95c0e4d3a5e2145f3d4949
priscilla-cassandra <priscilla-92@live.com>
2026-02-19 12:58:12 +0100

    carousel functionality

--- js/feed.js
+++ js/feed.js
@@ -2,7 +2,74 @@ import { BLOG_POSTS_URL } from "./api.js";
 const thumbnailContainer = document.getElementById("thumbnail-container")
 thumbnailContainer.classList.add("thumbnail-grid")
 
+const params = new URLSearchParams(window.location.search)
+const id = params.get("id")
 
+const carouselContainer = document.getElementById("carousel")
+const carouselImage = document.getElementById("carousel-post-image")
+const nextButton = document.getElementById("next-button")
+const previousButton = document.getElementById("back-button")
+const viewRecipeButton = document.getElementById("view-recipe-button")
+let posts = []
+let currentIndex = 0
+
+//----------CAROUSEL----------
+async function getThreeLatestPosts(){
+    try{
+        const response = await fetch(`${BLOG_POSTS_URL}?sort=created&sortOrder=desc&limit=3&page=1`)
+        const result = await response.json()
+
+        if(!response.ok){
+            throw new Error(result?.errors?.[0].message || response.status)
+        }
+
+        return result.data
+
+    }catch(error){
+        carouselContainer.textContent = "Unable to load latest recipes. Please try again"
+        console.log(error)
+    }
+}
+
+function renderSlide(){
+    const post = posts[currentIndex]
+    carouselImage.src = post.media?.url
+    carouselImage.alt = post.title
+    viewRecipeButton.href = `post/index.html?id=${post.id}`
+}
+
+function showCarouselImage(){
+    if(currentIndex >= posts.length) currentIndex = 0
+    if(currentIndex < 0) currentIndex = posts.length -1
+    renderSlide()
+}
+
+nextButton.addEventListener("click", ()=>{
+    currentIndex++
+    showCarouselImage()
+})
+previousButton.addEventListener("click", ()=>{
+    currentIndex --
+    showCarouselImage()
+})
+
+setInterval(()=>{
+    currentIndex++
+    showCarouselImage()
+}, 3000)
+
+async function startCarousel(){
+    posts = await getThreeLatestPosts()
+
+    if(!posts || posts.length === 0)
+        return
+
+    currentIndex = 0
+    showCarouselImage()
+}
+
+
+//----------THUMBNAIL GRID----------
 async function getBlogPostThumbnail(){
     try{
         const response = await fetch(BLOG_POSTS_URL)
@@ -10,13 +77,10 @@ async function getBlogPostThumbnail(){
 
         if(!response.ok){
             throw new Error(result?.errors?.[0].message || response.status)
-        }
+        }       
         
         const blogPosts = result.data
-        
-        console.log(result)
-
-
+    
         if(blogPosts.length === 0){
             thumbnailContainer.innerHTML = `<p> No posts yet! We are working on it`
         }
@@ -48,7 +112,6 @@ async function getBlogPostThumbnail(){
             thumbnailCard.appendChild(likes)
 
             thumbnailContainer.appendChild(thumbnailCard)
-
         })
 
     } catch(error){
@@ -57,3 +120,4 @@ async function getBlogPostThumbnail(){
 }
 
 getBlogPostThumbnail()
+startCarousel()
